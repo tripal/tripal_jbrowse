f875c78f653303f0de2846f425e542946ea965c3
Lacey Sanderson <lacey.sanderson@usask.ca>
2020-02-26 11:35:05 -0600

    Add analysis and description to the embedded page.

--- tripal_jbrowse_page/theme/jbrowse-instance--embedded.tpl.php
+++ tripal_jbrowse_page/theme/jbrowse-instance--embedded.tpl.php
@@ -1,4 +1,23 @@
+<?php
+/**
+ * Provides an embedded JBrowse instance.
+ *
+ * Variables:
+ *  - $url: the url of the JBrowse instance.
+ *  - $instance: an object describing the jbrowse instance.
+ */
+?>
 
+<?php if ($instance->analysis_id > 0) : ?>
+  <h3>Sequence Assembly:
+    <?php if ($instance->analysis->url) : ?>
+      <a href="<?php print $instance->analysis->url; ?>"><?php print $instance->analysis->name; ?></a>
+    <?php else: ?>
+      <?php print $instance->analysis->name; ?>
+    <?php endif; ?>
+  </h3>
+<?php endif; ?>
+<p><?php print $instance->description; ?></p>
 <div id="JBrowseInstance">
   <iframe src="<?php print $url;?>" width="100%" height="100%">
   </iframe>
